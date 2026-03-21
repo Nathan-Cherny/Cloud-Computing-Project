24917144544c2b2b0d74daad035e1652e2d983fa
Nathan Cherny <natec3632@gmail.com>
2026-03-21 17:20:32 -0400

    test commit, want to see how compare_image_similarity works

--- cards/image_similarity.py
+++ cards/image_similarity.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 
 def compare_image_similarity(query_signature: str, stored_signature: str) -> float:
+    print("test!")
+    print(query_signature, stored_signature)
     """
     TODO(student): compare two histogram signatures and return similarity in [0.0, 1.0].
     HINT: you can parse comma-separated floats and use cosine similarity.
