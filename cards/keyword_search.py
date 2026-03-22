33322bdca79e1ce1ff8b86fe6274ae6c82a0745d
Nathan Cherny <natec3632@gmail.com>
2026-03-22 14:25:02 -0400

    inplemented apply_keyword_search that will filter queryset by a criteria of icontains OR

--- cards/keyword_search.py
+++ cards/keyword_search.py
@@ -1,6 +1,16 @@
 from __future__ import annotations
-
+from functools import reduce
 
 def apply_keyword_search(queryset, keyword: str):
-    #TODO
-    return queryset
+    # filter query sets for attributes listed in attributes_to_check, then return merged queryset
+    attributes_to_check = ["title", "tags", "description"]
+    querysets = []
+    
+    for attribute in attributes_to_check:
+        arguments = {attribute+"__icontains": keyword}
+        querysets.append(queryset.filter(**arguments))
+    
+    return reduce(merge_query_sets, querysets)
+
+def merge_query_sets(qs1, qs2):
+    return qs1 | qs2
